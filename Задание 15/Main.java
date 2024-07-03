ccaa9aeb6363d6bade524cdc50093376b626fd5d
ZhamsoUlzutuev <zhamsoulzutuev1234@gmail.com>
2024-07-03 16:02:47 +0900

    Update Main.java

--- Задание 15/Main.java
+++ Задание 15/Main.java
@@ -6,21 +6,19 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) {
         try {
-            // Источник XML файла
+
             Source xmlSource = new StreamSource(new File("tariffs.xml"));
-            // Источник XSL файла
+
             Source xsltSource = new StreamSource(new File("transform.xsl"));
-            // Результат будет сохранен в tariffs.html
+
             Result result = new StreamResult(new File("tariffs.html"));
 
-            // Получаем трансформер
             TransformerFactory factory = TransformerFactory.newInstance();
             Transformer transformer = factory.newTransformer(xsltSource);
 
-            // Выполняем трансформацию
             transformer.transform(xmlSource, result);
 
-            System.out.println("Transformation completed successfully.");
+            System.out.println("Трансформация проведена успешно.!");
         } catch (Exception e) {
             e.printStackTrace();
         }
